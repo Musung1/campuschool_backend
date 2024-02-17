c6930a9436cf2b2511ee98f8d631c1a55d24fb8e
musung <musungkim1352@gmail.com>
2024-02-18 00:18:07 +0900

    first commit

---
+++ src/main/java/com/example/campuschool_backend/CampuschoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.campuschool_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuschoolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampuschoolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/campuschool_backend/CampuschoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campuschool_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuschoolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
