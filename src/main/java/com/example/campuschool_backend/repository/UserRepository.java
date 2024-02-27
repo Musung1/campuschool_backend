deee67136954718bd499faaae5082ec3ada6336c
musung <musungkim1352@gmail.com>
2024-02-27 16:30:52 +0900

    [Feat] 스프링 시큐리티 관련 설정 구현 #17

--- src/main/java/com/example/campuschool_backend/repository/UserRepository.java
+++ src/main/java/com/example/campuschool_backend/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.campuschool_backend.repository;
 import com.example.campuschool_backend.domain.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    Optional<UserEntity> findByUsername(String username);
 }
